6e9a7baf4f17553156d35e36feef41d9a37c5cc0
你的GitHub用户名 <GitHub>
2025-11-15 17:59:23 +0800

    优化-2

--- 3D-games/vite.config.js
+++ 3D-games/vite.config.js
@@ -20,13 +20,7 @@ export default defineConfig({
     },
   },
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
